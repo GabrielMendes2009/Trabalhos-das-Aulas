6dfb3c3f755a1ba544a0fa3793f2a9f3c135189a
GabrielMendes2009 <gabriel.alves.mendes2009@gmail.com>
2026-04-13 11:24:18 +0100

    Refactor arithmetic print statements for consistency in spacing

--- 10795 - Segurança no desenvolvimento de software/Ex1/ex8.py
+++ 10795 - Segurança no desenvolvimento de software/Ex1/ex8.py
@@ -4,16 +4,16 @@ n2 = float(input("Número 2: "))
 
 match operacao:
     case "soma":
-        print(n1 + n2)
+        print(n1+n2)
 
     case "subtrai":
-        print(n1 - n2)
+        print(n1-n2)
 
     case "multiplica":
-        print(n1 * n2)
+        print(n1*n2)
 
     case "divide" if n2 != 0:
-        print(n1 / n2)
+        print(n1/n2)
 
     case "divide":
         print("Erro: divisão por zero")
