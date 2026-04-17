b3ae43b28242669925e8b7e0a07ddc9e951b47ee
Galabriel <gabriel.alves.mendes2009@gmail.com>
2026-04-17 10:34:35 +0100

    Fix input prompt and improve calculations in ex3_12.py

--- 10795 - Segurança no desenvolvimento de software/Ex3/ex3_12.py
+++ 10795 - Segurança no desenvolvimento de software/Ex3/ex3_12.py
@@ -1,4 +1,5 @@
 n = int(input("Introduza um número inteiro positivo: "))
+
 if n <= 1:
     print("Por favor, introduza um número maior que 1.")
 else:
@@ -6,11 +7,13 @@ else:
     resultado_sub = 0
     resultado_mult = 1
     total_iter = 0
+
     for val in range(1, n):
         resultado_soma = resultado_soma + val
         resultado_sub = resultado_sub - val
         resultado_mult = resultado_mult * val
         total_iter = total_iter + 1
+    
     print("\n=== Resultados para o número " + str(n) + " ===")
     print("Soma: " + str(resultado_soma))
     print("Subtração: " + str(resultado_sub))
