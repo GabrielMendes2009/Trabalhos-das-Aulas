5f7d225375bb1ad2bf268e8e4eaf4b33c431998a
Galabriel <gabriel.alves.mendes2009@gmail.com>
2026-04-17 10:32:27 +0100

    Fix print formatting for saldo_novo variable

--- 10795 - Segurança no desenvolvimento de software/Ex2/ex2_4.py
+++ 10795 - Segurança no desenvolvimento de software/Ex2/ex2_4.py
@@ -3,6 +3,6 @@ valor_cheque = float(input("Valor de cheque: "))
 
 if valor_cheque <= saldo_conta:
     saldo_novo = saldo_conta - valor_cheque
-    print(f"Cheque descontado\nSaldo: {saldo_novo:}")
+    print(f"Cheque descontado\nSaldo: {saldo_novo}")
 else:
     print("Cheque não pode ser Descontado")
