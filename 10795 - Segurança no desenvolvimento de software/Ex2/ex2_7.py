360290b12d70205c08d1444353d69afa83d2a56b
Galabriel <gabriel.alves.mendes2009@gmail.com>
2026-04-17 10:33:03 +0100

    Calculate final average and determine approval status

--- 10795 - Segurança no desenvolvimento de software/Ex2/ex2_7.py
+++ 10795 - Segurança no desenvolvimento de software/Ex2/ex2_7.py
@@ -1,6 +1,8 @@
 nota_a = float(input("Nota1 = "))
 nota_b = float(input("Nota2 = "))
 nota_c = float(input("Nota3 = "))
+
 media_final = (nota_a * 2 + nota_b * 3 + nota_c * 5) / 10
+
 print(f"Média: {media_final:.1f}")
 print("Aprovado" if media_final >= 6 else "Reprovado")
