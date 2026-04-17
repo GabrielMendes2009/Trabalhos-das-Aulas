fb174da9ada5e891c7d6d13e23eaecb28707eaa5
Galabriel <gabriel.alves.mendes2009@gmail.com>
2026-04-17 10:32:12 +0100

    Fix print formatting for purchase details

--- 10795 - Segurança no desenvolvimento de software/Ex2/ex2_6.py
+++ 10795 - Segurança no desenvolvimento de software/Ex2/ex2_6.py
@@ -1,13 +1,16 @@
 cliente = input("Nome do cliente: ")
 valor_compra = float(input("Valor da compra: "))
+
 if valor_compra <= 200:
     perc_desconto = valor_compra * 0.10
 elif valor_compra <= 500:
     perc_desconto = valor_compra * 0.15
 else:
     perc_desconto = valor_compra * 0.20
+
 valor_final = valor_compra - perc_desconto
+
 print(f"Nome: {cliente}")
-print(f"Compra: {valor_compra:.2f}€")
-print(f"Desconto: {perc_desconto:.2f}€")
-print(f"Total a pagar: {valor_final:.2f}€")
+print(f"Compra: {valor_compra}€")
+print(f"Desconto: {perc_desconto}€")
+print(f"Total a pagar: {valor_final}€")
