bd5fda4e6ff1d2621273529b0998ef410c12d0a8
Galabriel <gabriel.alves.mendes2009@gmail.com>
2026-04-17 10:36:12 +0100

    Fix formatting and improve output for multiples of 5

--- 10795 - Segurança no desenvolvimento de software/Ex3/ex3_17.py
+++ 10795 - Segurança no desenvolvimento de software/Ex3/ex3_17.py
@@ -1,5 +1,6 @@
 print("Múltiplos de 5 que não são múltiplos de 3:\n")
 total = 0
+
 for num in range(1, 1001):
     if num % 5 == 0 and num % 3 != 0:
         print(str(num), end="  ")
@@ -7,4 +8,5 @@ for num in range(1, 1001):
 
         if total % 10 == 0:
             print()
+
 print("\n\nTotal de números encontrados: " + str(total))
