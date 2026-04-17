ccbba5d1d3e834ba9f2b7f91d751a8fbb02dc655
Galabriel <gabriel.alves.mendes2009@gmail.com>
2026-04-17 10:37:53 +0100

    Add logic to find the first 10 prime numbers

--- 10795 - Segurança no desenvolvimento de software/Ex3/ex3_6.py
+++ 10795 - Segurança no desenvolvimento de software/Ex3/ex3_6.py
@@ -1,7 +1,9 @@
 lista_primos = []
 candidato = 2
+
 while len(lista_primos) < 10:
     if all(candidato % d != 0 for d in range(2, int(candidato**0.5) + 1)):
         lista_primos.append(candidato)
     candidato += 1
+
 print(lista_primos)
