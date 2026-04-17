20dc77880fd0db4ff7a9b055cb027add39db112d
Galabriel <gabriel.alves.mendes2009@gmail.com>
2026-04-17 10:37:00 +0100

    Update ex3_2.py

--- 10795 - Segurança no desenvolvimento de software/Ex3/ex3_2.py
+++ 10795 - Segurança no desenvolvimento de software/Ex3/ex3_2.py
@@ -1,9 +1,11 @@
 qtd_pares = qtd_impares = 0
+
 for _ in range(10):
     valor = int(input("Número: "))
     if valor % 2 == 0:
         qtd_pares += 1
     else:
         qtd_impares += 1
+
 print(f"Pares: {qtd_pares}")
 print(f"Ímpares: {qtd_impares}")
