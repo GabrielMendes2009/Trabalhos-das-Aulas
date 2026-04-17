8d6f7ad7a57d029b52f1b473c246ab3b4ffb2d2a
Galabriel <gabriel.alves.mendes2009@gmail.com>
2026-04-17 10:31:40 +0100

    Add sorting and printing of values in order

--- 10795 - Segurança no desenvolvimento de software/Ex2/ex2_5.py
+++ 10795 - Segurança no desenvolvimento de software/Ex2/ex2_5.py
@@ -1,4 +1,5 @@
 valores = [int(input(f"num{i+1} = ")) for i in range(3)]
 valores_ord = sorted(valores)
+
 print(f"Crescente: {valores_ord[0]}, {valores_ord[1]}, {valores_ord[2]}")
 print(f"Decrescente: {valores_ord[2]}, {valores_ord[1]}, {valores_ord[0]}")
