c6541447ee438ecbc38e4edaacf0fb5da912faea
Galabriel <gabriel.alves.mendes2009@gmail.com>
2026-04-17 10:32:50 +0100

    Update ex2_6.py

--- 10795 - Segurança no desenvolvimento de software/Ex2/ex2_6.py
+++ 10795 - Segurança no desenvolvimento de software/Ex2/ex2_6.py
@@ -2,11 +2,11 @@ cliente = input("Nome do cliente: ")
 valor_compra = float(input("Valor da compra: "))
 
 if valor_compra <= 200:
-    perc_desconto = valor_compra * 0.10
+    perc_desconto = valor_compra*0.10
 elif valor_compra <= 500:
-    perc_desconto = valor_compra * 0.15
+    perc_desconto = valor_compra*0.15
 else:
-    perc_desconto = valor_compra * 0.20
+    perc_desconto = valor_compra*0.20
 
 valor_final = valor_compra - perc_desconto
 
