f3cefbcf77c637147722180e844d7db4929dfc4b
Galabriel <gabriel.alves.mendes2009@gmail.com>
2026-04-17 10:14:13 +0100

    Fix input prompt and improve output formatting

--- 10795 - Segurança no desenvolvimento de software/Ex2/ex2_1.py
+++ 10795 - Segurança no desenvolvimento de software/Ex2/ex2_1.py
@@ -1,5 +1,7 @@
 total_seg = int(input("Informe a quantidade de segundos: "))
+
 horas = total_seg // 3600
 minutos = (total_seg % 3600) // 60
 segundos_rest = total_seg % 60
+
 print(f"{horas} hora(s), {minutos} minuto(s) e {segundos_rest} segundo(s)")
