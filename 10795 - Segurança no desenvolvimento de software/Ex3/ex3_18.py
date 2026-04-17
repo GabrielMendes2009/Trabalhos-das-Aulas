9fcc59046ca512a8914823dc17288fcd6433f0b1
Galabriel <gabriel.alves.mendes2009@gmail.com>
2026-04-17 10:36:31 +0100

    Update ex3_18.py

--- 10795 - Segurança no desenvolvimento de software/Ex3/ex3_18.py
+++ 10795 - Segurança no desenvolvimento de software/Ex3/ex3_18.py
@@ -1,6 +1,7 @@
 teto = int(input("Até que número quer verificar números perfeitos? "))
 print("\nNúmeros perfeitos encontrados até " + str(teto) + ":")
 total_perfeitos = 0
+
 for num in range(1, teto + 1):
     soma_div = 0
     for d in range(1, num):
@@ -9,4 +10,5 @@ for num in range(1, teto + 1):
     if soma_div == num:
         print(str(num) + " é um número perfeito!")
         total_perfeitos = total_perfeitos + 1
+
 print("\nTotal de números perfeitos encontrados: " + str(total_perfeitos))
